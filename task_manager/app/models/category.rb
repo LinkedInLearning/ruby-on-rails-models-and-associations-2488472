65654a98cd901af2be710480da4a387024d88d9e
Kevin Skoglund <kevin@novafabrica.com>
2022-06-05 16:36:00 -0400

    Add code for 02_02

--- task_manager/app/models/category.rb
+++ task_manager/app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
 
   has_many :tasks
 
+  validates_length_of :name, within: 2..50
+
   scope :sorted, -> { order(:name) }
 
 end
--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -2,6 +2,11 @@ class Task < ApplicationRecord
 
   belongs_to :category, optional: true
 
+  validates_presence_of :name
+  validates_length_of :name, maximum: 50
+  validates_presence_of :position
+  validates_numericality_of :position, greater_than: 0
+
   scope :complete, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: false) }
   scope :sorted, -> { order(:position) }
