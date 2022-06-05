837540b718bbf5f81128a9e2ec7c2ee4b436e74b
Kevin Skoglund <kevin@novafabrica.com>
2022-06-05 17:08:31 -0400

    Finish movie 02_04

--- task_manager/app/models/task.rb
+++ task_manager/app/models/task.rb
@@ -7,9 +7,23 @@ class Task < ApplicationRecord
   validates_presence_of :position
   validates_numericality_of :position, greater_than: 0
 
+  validate :description_has_no_prohibited_words
+
   scope :complete, -> { where(completed: true) }
   scope :incomplete, -> { where(completed: false) }
   scope :sorted, -> { order(:position) }
   scope :search, -> (kw) { where("LOWER(name) LIKE ?", "%#{kw.downcase}%")}
+
+  private
+
+    def description_has_no_prohibited_words
+      return unless description.present?
+      prohibited_words = ['later', 'eventually', 'someday']
+      prohibited_words.each do |word|
+        if description.include?(word)
+          errors.add(:description, "cannot contain prohibited word: #{word}")
+        end
+      end
+    end
 
 end
