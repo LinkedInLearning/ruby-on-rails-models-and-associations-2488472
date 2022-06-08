94ec78a9886db5581ecf053595f18624cd6346a7
Kevin Skoglund <kevin@novafabrica.com>
2022-06-08 11:27:46 -0400

    Finish movie 04_04

--- task_manager/app/models/category.rb
+++ task_manager/app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
-  has_many :tasks
+  has_many :tasks, dependent: :nullify
 
   validates_length_of :name, within: 2..50
 
--- task_manager/db/task_manager_development.sql
+++ task_manager/db/task_manager_development.sql
@@ -24,7 +24,7 @@ CREATE TABLE `categories` (
   `created_at` datetime(6) NOT NULL,
   `updated_at` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 INSERT INTO `categories` VALUES (1,'Weekly','2022-04-25 15:31:37.175665','2022-04-25 15:31:37.175665'),(2,'Monthly','2022-06-08 15:12:39.628498','2022-06-08 15:12:39.628498');
@@ -51,10 +51,10 @@ CREATE TABLE `tasks` (
   `updated_at` datetime(6) NOT NULL,
   `category_id` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO `tasks` VALUES (1,'Fold laundry','Fold the laundry in the basket and put it in the drawers',1,0,'2022-04-25 14:21:23.314635','2022-04-26 20:39:21.558599',1),(2,'Sweep Porch','Sweep dirt off the porch',2,0,'2022-04-25 14:22:30.251955','2022-06-08 15:14:03.980789',2),(4,'Wash Dishes','Wash and dry the dishes',3,0,'2022-04-26 20:11:20.883374','2022-06-08 15:14:03.982904',2),(6,'Mow Lawn','Get the lawn mower out of the garage. Clean underside around the blades. Fill the gas tank. Adjust the mower height. Set mower to mulch. Make sure the yard is clear of branches, toys, etc. Mow in different directions each week. Edge and sweep borders and pathways.',4,1,'2022-05-27 20:52:01.475690','2022-06-08 15:16:47.256843',2),(7,'Rake Leaves','Rake the leaves in the front and back yard',5,NULL,'2022-06-05 21:24:36.745008','2022-06-05 21:25:44.570912',NULL),(8,'Wash car',NULL,6,NULL,'2022-06-05 21:38:01.924070','2022-06-05 21:38:45.594382',NULL);
+INSERT INTO `tasks` VALUES (1,'Fold laundry','Fold the laundry in the basket and put it in the drawers',1,0,'2022-04-25 14:21:23.314635','2022-04-26 20:39:21.558599',1),(2,'Sweep Porch','Sweep dirt off the porch',2,0,'2022-04-25 14:22:30.251955','2022-06-08 15:14:03.980789',2),(4,'Wash Dishes','Wash and dry the dishes',3,0,'2022-04-26 20:11:20.883374','2022-06-08 15:14:03.982904',2),(6,'Mow Lawn','Get the lawn mower out of the garage. Clean underside around the blades. Fill the gas tank. Adjust the mower height. Set mower to mulch. Make sure the yard is clear of branches, toys, etc. Mow in different directions each week. Edge and sweep borders and pathways.',4,1,'2022-05-27 20:52:01.475690','2022-06-08 15:16:47.256843',2),(7,'Rake Leaves','Rake the leaves in the front and back yard',5,NULL,'2022-06-05 21:24:36.745008','2022-06-08 15:26:06.334204',NULL),(8,'Wash car',NULL,6,NULL,'2022-06-05 21:38:01.924070','2022-06-05 21:38:45.594382',NULL);
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
